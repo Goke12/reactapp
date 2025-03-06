46896e6cbbffa799a54bf093d19917b3fc874712
izudeen <izudeenadegoke@gmail.com>
2025-03-06 23:45:33 +0000

    second stage

--- src/blog/Meal.jsx
+++ src/blog/Meal.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 const Meal = ()=>{
     let [count, setCount] = useState(0);
     let [meals, setMeals] = useState([]);
+    let [errormsg, setErrorMsg] = useState(null);
+    let [loader, setLoader] = useState(false);
+
+    
     let fetchMeal = ()=>{
         fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
         .then((response)=>{
@@ -15,16 +19,19 @@ const Meal = ()=>{
             //we are keeping the data received into variable meals
         })
         .catch((error)=>{
+            setErrorMsg(error.message);
             console.log(error);
         })
+        .finally(()=>{setLoader(false)})
         
     }
     //useEffect(()=>{codes here will execute anytime the page renders},[])
     useEffect(()=>{
+        setLoader(true);
         fetchMeal();
         console.log(meals)
        
-    },[meals]);
+    },[errormsg]);
     let  meal2display = meals.map(function(value,key){
         return <div className="row py-3">
         <div className="col-md-4">
@@ -41,7 +48,13 @@ const Meal = ()=>{
         
                 <h1 className="my-4">Yummy Meal Categories</h1>
                 {/* Row */}
-                {(meals.length > 0)? meal2display : <div className="text-center"><div className="spinner-border" role='status'><span className="visually-hidden">Loading...</span></div></div>}
+                {loader &&  <div className="text-center"><div className="spinner-border" role='status'><span className="visually-hidden">Loading...</span></div></div>}
+                {errormsg &&  <div className="alert alert-danger">
+                <p>{errormsg}</p>
+                <button className="btn btn-danger" onClick={()=>{setErrorMsg(false)}}>Refresh</button>
+                </div>}
+
+                {meals && meal2display}
            
             {/* End Row */}
         
